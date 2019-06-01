87e380b62a6eef72ae260d0e8811f8db82bfd538
Daniel <daniel_kish@outlook.com>
2019-06-01 22:04:40 +0300

    Rework build.

--- tests/test_read_write.py
+++ tests/test_read_write.py
@@ -7,5 +7,11 @@ bin_dir = os.path.join(proj_dir, 'build\\Debug')
 reader_exe = os.path.join(bin_dir, 'reader.exe')
 writer_exe = os.path.join(bin_dir, 'writer.exe')
 
-os.system(reader_exe + ' data.txt')
-os.system(writer_exe + ' person.bin')
+with open('data.txt', 'w') as f:
+    f.write('John Watson Doctor')
+
+os.system(writer_exe + ' data.txt')
+os.system(reader_exe + ' person.bin')
+
+os.remove('data.txt')
+os.remove('person.bin')
